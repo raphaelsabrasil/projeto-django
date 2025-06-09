9641a06cd7379c61cd54ef99926842f1587b3390
Raphael Sá Brasil <raphael7283@gmail.com>
2025-06-08 20:23:15 -0400

    Criando templates

--- recipes/views.py
+++ recipes/views.py
@@ -1,12 +1,14 @@
-#from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return HttpResponse('HOME 2')
+    return render(request, 'recipes/home.html', context={
+        'name': 'Raphael Sá Brasil',
+    }, status=201)
 
 def contato(request):
-    return HttpResponse('contato')
+    return render(request, 'recipes/contato.html')
 
 def sobre(request):
     return HttpResponse('sobre')
