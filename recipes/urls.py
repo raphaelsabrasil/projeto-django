65a4a4085863854b55eb49aba43a2a5bfe8dc223
Raphael Sá Brasil <raphael7283@gmail.com>
2025-07-05 00:11:14 -0400

    App_name para namespace

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 #from recipes.views import home
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('recipes/<int:id>/', views.recipe),
+    path('', views.home, name="home"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
+
+# path('', views.home, name="recipes-home") <<< usado caso não defina um app_name
