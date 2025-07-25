3b67906fb480af17cdff93c8116239bd3c2217d3
Raphael Sá Brasil <raphael7283@gmail.com>
2025-07-24 23:02:19 -0400

    Mostrando erro 404 e usando shortcut get_list_or_404

--- recipes/views.py
+++ recipes/views.py
@@ -1,27 +1,33 @@
 #from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
 from utils.recipes.factory import make_recipe 
+# from django.http import Http404
 
-from .models import Recipe
+from recipes.models import Recipe
 
 # Create your views here.
 
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
-    ).order_by('-id')
+    ).order_by('-id')    
     return render(request, 'recipes/pages/home.html', context={
-        'recipes':recipes,
+        'recipes':recipes,        
     })
 
-def category(request, category_id):
-    recipes = Recipe.objects.filter(
-        category__id=category_id,
-        is_published=True,
-    ).order_by('-id')
+
+def category(request, category_id):    
+    recipes = get_list_or_404(
+        Recipe.objects.filter(
+            category__id=category_id,
+            is_published=True,
+        ).order_by('-id')
+    )
     return render(request, 'recipes/pages/category.html', context={
         'recipes':recipes,
+        'title': f'{recipes[0].category.name} - Category | '
     })
+
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
@@ -30,7 +36,24 @@ def recipe(request, id):
     })
 
 
+
+
 # def home(request):
 #     return render(request, 'recipes/pages/home.html', context={
 #         'recipes': [make_recipe() for _ in range(10)],  #list comprehension que gera 10 receitas, sem variável
 #     }, status=201)
+
+
+
+# def category(request, category_id):
+#     recipes = Recipe.objects.filter(
+#         category__id=category_id,
+#         is_published=True,
+#     ).order_by('-id')
+
+#     if not recipes:
+#         raise Http404('Not found 🥲')    
+#     return render(request, 'recipes/pages/category.html', context={
+#         'recipes':recipes,
+#         'title': f'{recipes.first().category.name} - Category | '
+#     })
