6ba376ca662fee19adc80ee96359fc2cee95eb0d
Raphael Sá Brasil <raphael7283@gmail.com>
2025-08-05 22:11:15 -0400

    Criando fixtures e testando context e content

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse, resolve
-
 from recipes import views
+from recipes.models import Category, Recipe, User
 # Create your tests here.
 
 class RecipeViewsTest(TestCase):
@@ -24,6 +24,42 @@ class RecipeViewsTest(TestCase):
             '<h1>No recipes found here 🥲</h1>',        
             response.content.decode('utf-8')
         )
+
+    # Fixtures
+    def test_recipe_home_template_loads_recipes(self):
+        category = Category.objects.create(name='Category')
+        author = User.objects.create_user(
+            first_name='user',
+            last_name='name',
+            username='username',
+            password='123456',
+            email='username@email.com',
+        )
+        recipe = Recipe.objects.create(
+            category=category,
+            author=author,
+            title = 'Recipe Title',
+            description = 'Recipe Description',
+            slug = 'recipe-slug',
+            preparation_time = 10,
+            preparation_time_unit = 'Minutos',
+            servings = 5,
+            servings_unit = 'Porções',
+            preparation_steps = 'Recipe Preparation Steps',
+            preparation_steps_is_html = False,            
+            is_published = True,            
+        )
+        # Teste
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        self.assertIn('Recipe Title', content)
+        self.assertIn('Recipe Description', content)
+        self.assertIn('10 Minutos', content)
+        self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)      # quantidade de receitas
+        
 
 
     def test_recipe_category_view_function_is_correct(self):
