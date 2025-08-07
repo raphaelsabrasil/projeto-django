bb4821fb7854ff1624afa617769f44863be6b1c9
Raphael Sá Brasil <raphael7283@gmail.com>
2025-08-06 23:22:18 -0400

    Criando classe de base e extraindo métodos

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,14 +1,19 @@
-from django.test import TestCase
+#from django.test import TestCase
 from django.urls import reverse, resolve
 from recipes import views
-from recipes.models import Category, Recipe, User
-# Create your tests here.
+#from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase, Recipe
 
-class RecipeViewsTest(TestCase):
+
+class RecipeViewsTest(RecipeTestBase):
+    # def tearDown(self) -> None:
+    #     return super().tearDown()
+    
+    # SETUP
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)    
-        
+    # TEARDOWN
 
     def test_recipe_home_view_returns_status_code_200_OK(self):
         response = self.client.get(reverse('recipes:home'))
@@ -19,6 +24,7 @@ class RecipeViewsTest(TestCase):
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
 
     def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        # Recipe.objects.get(pk=1).delete()
         response = self.client.get(reverse('recipes:home'))
         self.assertIn(      # quer saber se 'no recipe found' está no código.
             '<h1>No recipes found here 🥲</h1>',        
@@ -26,38 +32,19 @@ class RecipeViewsTest(TestCase):
         )
 
     # Fixtures
-    def test_recipe_home_template_loads_recipes(self):
-        category = Category.objects.create(name='Category')
-        author = User.objects.create_user(
-            first_name='user',
-            last_name='name',
-            username='username',
-            password='123456',
-            email='username@email.com',
-        )
-        recipe = Recipe.objects.create(
-            category=category,
-            author=author,
-            title = 'Recipe Title',
-            description = 'Recipe Description',
-            slug = 'recipe-slug',
-            preparation_time = 10,
-            preparation_time_unit = 'Minutos',
-            servings = 5,
-            servings_unit = 'Porções',
-            preparation_steps = 'Recipe Preparation Steps',
-            preparation_steps_is_html = False,            
-            is_published = True,            
-        )
-        # Teste
+    def test_recipe_home_template_loads_recipes(self):               
+        # Need a recipe for this test
+        self.make_recipe()
+
         response = self.client.get(reverse('recipes:home'))
         content = response.content.decode('utf-8')
         response_context_recipes = response.context['recipes']
 
+        # Check if one recipe exists
         self.assertIn('Recipe Title', content)
-        self.assertIn('Recipe Description', content)
-        self.assertIn('10 Minutos', content)
-        self.assertIn('5 Porções', content)
+        # self.assertIn('Recipe Description', content)
+        # self.assertIn('10 Minutos', content)
+        # self.assertIn('5 Porções', content)
         self.assertEqual(len(response_context_recipes), 1)      # quantidade de receitas
         
 
