aa40d6b35b32bb14ef047ce3511512f42d63ccb4
Raphael Sá Brasil <raphael7283@gmail.com>
2025-08-16 20:53:24 -0400

    Teste para escapar por segurança e consultas complexas com Q

---
+++ recipes/tests/test_recipe_home_views.py
@@ -0,0 +1,64 @@
+#from django.test import TestCase
+from django.urls import reverse, resolve
+from recipes import views
+#from recipes.models import Category, Recipe, User
+from .test_recipe_base import RecipeTestBase
+from unittest import skip
+
+
+class RecipeHomeViewTest(RecipeTestBase):
+    # def tearDown(self) -> None:
+    #     return super().tearDown()
+    
+    # SETUP
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)    
+    # TEARDOWN
+
+    def test_recipe_home_view_returns_status_code_200_OK(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_recipe_home_view_loads_correct_template(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    # @skip('Usando skip para pular o teste')
+    def test_recipe_home_template_shows_no_recipes_found_if_no_recipes(self):
+        # Recipe.objects.get(pk=1).delete()
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn(      # quer saber se 'no recipe found' está no código.
+            '<h1>No recipes found here 🥲</h1>',        
+            response.content.decode('utf-8')
+        )
+        # self.fail('Para que eu termine de digitá-lo')   # para falhar um teste
+
+    # Fixtures
+    def test_recipe_home_template_loads_recipes(self):               
+        # Need a recipe for this test
+        self.make_recipe()
+
+        response = self.client.get(reverse('recipes:home'))
+        content = response.content.decode('utf-8')
+        response_context_recipes = response.context['recipes']
+
+        # Check if one recipe exists
+        self.assertIn('Recipe Title', content)
+        # self.assertIn('Recipe Description', content)
+        # self.assertIn('10 Minutos', content)
+        # self.assertIn('5 Porções', content)
+        self.assertEqual(len(response_context_recipes), 1)      # quantidade de receitas
+        
+    def test_recipe_home_template_dont_load_recipes_not_published(self):
+        """Test recipe is_published False dont show"""
+        # Need a recipe for this test
+        self.make_recipe(is_published=False)
+
+        response = self.client.get(reverse('recipes:home'))
+
+        # Check if one recipe exists
+        self.assertIn(
+            '<h1>No recipes found here 🥲</h1>',
+            response.content.decode('utf-8')
+        )
--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import get_list_or_404, get_object_or_404, render
 from django.http.response import Http404
 # from utils.recipes.factory import make_recipe
 # from django.http import Http404
+from django.db.models import Q      # >> usado para consultas complexas no django
 
 from recipes.models import Recipe
 
@@ -44,14 +45,25 @@ def recipe(request, id):
     })
 
 def search(request):
-    search_term = request.GET.get('q', '').strip()
+    search_term = request.GET.get('q', '').strip()  # strip >> função python para remover espaços
 
+    # caso procure vazio, levanta erro 404
     if not search_term:
         raise Http404()
-    
+
+    # procurar entre os titulos OU descrições a palavra digitada no campo search
+    recipes = Recipe.objects.filter(
+        Q(
+            Q(title__icontains=search_term) |
+            Q(description__icontains=search_term),
+        ),
+        is_published=True
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for "{search_term}" |',
         'search_term': search_term,
+        'recipes': recipes,
     })
 
 
