df50af18c550d1fe45962793a7ae6d99547cf5ae
Raphael Sá Brasil <raphael7283@gmail.com>
2025-08-28 22:19:39 -0400

    Enviando post para outra url e usando session

--- authors/views.py
+++ authors/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from .forms import RegisterForm
 
 # Create your views here.
-def register_view(request):
-    # return render(request, 'authors/pages/register_view.html')
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+def register_view(request):     # para exibir os dados no formulário
+    # return render(request, 'authors/pages/register_view.html')   
+    # request.session['number']  = request.session.get('number') or 1
+    # request.session['number'] += 1
+    register_form_data = request.session.get('register_form_data', None)
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+def register_create(request):   # para tratar os dados
+    if not request.POST:
+        raise Http404()
+    
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+
+    return redirect('authors:register')
+
+    # return render(request, 'authors/pages/register_view.html', {
+    #     'form': form,
+    # })
