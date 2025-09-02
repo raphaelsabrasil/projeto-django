62dec49503a9e5f9c092cc09e41037b3489f74ef
Raphael Sá Brasil <raphael7283@gmail.com>
2025-09-02 00:17:27 -0400

    Usando validators e is_valid para salvar dados

--- authors/forms.py
+++ authors/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+import re
 
 def add_attr(field, attr_name, attr_new_val):
     existing = field.widget.attrs.get(attr_name, '')
@@ -9,6 +10,19 @@ def add_attr(field, attr_name, attr_new_val):
 def add_placeholder(field, placeholder_val):
     # field.widget.attrs['placeholder'] = placeholder_val
     add_attr(field, 'placeholder', placeholder_val)
+
+def strong_password(password):
+    regex = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$')
+
+    if not regex.match(password):
+        raise ValidationError((
+            'Password must have at least one uppercase letter,'
+            'one lowercase letter and one number. The length should be '
+            'at least 8 characters.'
+        ),
+            code='invalid'
+        )
+
 
 
 class RegisterForm(forms.ModelForm):
@@ -32,7 +46,8 @@ class RegisterForm(forms.ModelForm):
             'Password must have at least one uppercase letter, '
             'one lowercase letter and one number. The length should be '
             'at least 8 characters.'
-        )
+        ),
+        validators=[strong_password]
     )
     password2 = forms.CharField(
         required=True,
@@ -57,7 +72,7 @@ class RegisterForm(forms.ModelForm):
         labels = {
             'username': 'Username',
             'first_name': 'First name',
-            'last_name': 'Last name',            
+            'last_name': 'Last name',
             'email': 'E-mail',
             'password': 'Password',
         }
@@ -67,14 +82,14 @@ class RegisterForm(forms.ModelForm):
         error_messages = {
             'username': {
                 'required': 'This field must not be empty',
-                'max_length': 'This field must have less than 3 characteres',
-                'invalid': 'This field is invalid'
+                # 'max_length': 'This field must have less than 3 characteres',
+                # 'invalid': 'This field is invalid'
             }
         }
         widgets = {
             'first_name': forms.TextInput(attrs={
                 'placeholder': 'Type your username here',
-                'class': 'input text-input outra-classe'
+                'class': 'input text-input'
             }),
             'password': forms.PasswordInput(attrs={
                 'placeholder': 'Type your password here'
@@ -101,6 +116,7 @@ class RegisterForm(forms.ModelForm):
                 code='invalid',
                 params={'value': '"John Doe"'}
             )
+        return data
         
     def clean(self):
         cleaned_data = super().clean()
