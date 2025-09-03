cc1cf01a87e245ec5f7f7dcdfe87ad86557fa702
Raphael Sá Brasil <raphael7283@gmail.com>
2025-09-02 23:15:27 -0400

    Criando testes para form com placeholder e help_text

--- authors/forms.py
+++ authors/forms.py
@@ -32,13 +32,16 @@ class RegisterForm(forms.ModelForm):
         add_placeholder(self.fields['email'], 'Your e-mail')
         add_placeholder(self.fields['first_name'], 'Ex.: John')
         add_placeholder(self.fields['last_name'], 'Ex.: Doe')
-        add_attr(self.fields['username'], 'css', 'a-css-class')
+        # add_attr(self.fields['username'], 'css', 'a-css-class')
+        add_placeholder(self.fields['password'], 'Type your password')
+        add_placeholder(self.fields['password2'], 'Repeat your password')
 
     password = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Your password'
-        }),
+        # widget=forms.PasswordInput(attrs={
+        #     'placeholder': 'Your password'
+        # }),
+        widget=forms.PasswordInput(),
         error_messages={
             'required': 'Password must not be empty'
         },
@@ -51,9 +54,10 @@ class RegisterForm(forms.ModelForm):
     )
     password2 = forms.CharField(
         required=True,
-        widget=forms.PasswordInput(attrs={
-            'placeholder': 'Repeat your password'
-        })
+        # widget=forms.PasswordInput(attrs={
+        #     'placeholder': 'Repeat your password'
+        # })
+        widget=forms.PasswordInput(),
     )
 
 
@@ -86,18 +90,18 @@ class RegisterForm(forms.ModelForm):
                 # 'invalid': 'This field is invalid'
             }
         }
-        widgets = {
-            'first_name': forms.TextInput(attrs={
-                'placeholder': 'Type your username here',
-                'class': 'input text-input'
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Type your password here'
-            })
-        }
+        # widgets = {
+        #     'first_name': forms.TextInput(attrs={
+        #         'placeholder': 'Type your username here',
+        #         'class': 'input text-input'
+        #     }),
+        #     'password': forms.PasswordInput(attrs={
+        #         'placeholder': 'Type your password here'
+        #     })
+        # }
 
     def clean_password(self):
-        data=self.cleaned_data.get('password')
+        data = self.cleaned_data.get('password')
 
         if 'atenção' in data:
             raise ValidationError(
